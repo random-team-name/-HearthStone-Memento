223707aeca3e5ad19fbd4a48926657e846fd1b85
clabroche <corentinlabroche@gmail.com>
2018-05-11 12:15:15 +0000

    prepare order

--- angular/src/app/providers/hearthsone-cards.service.ts
+++ angular/src/app/providers/hearthsone-cards.service.ts
@@ -1,6 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
+/**
+ * card = { 
+ *   cardId
+ *   cardSet
+ *   collectible
+ *   dbfId
+ *   faction
+ *   health
+ *   img
+ *   imgGold
+ *   locale
+ *   name
+ *   playerClass
+ *   rarity
+ *   type
+ * }
+ */
 @Injectable({
   providedIn: 'root'
 })
